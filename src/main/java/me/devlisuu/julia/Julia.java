3029a6dc67291c98030758e73c654c6f698d8ed5
Lisu <everyaccountholder@wp.pl>
2022-05-08 19:25:56 +0200

    Add gradle project

---
+++ src/main/java/me/devlisuu/julia/Julia.java
@@ -0,0 +1,12 @@
+package me.devlisuu.julia;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Julia {
+    public static final Logger LOGGER = LoggerFactory.getLogger("Julia");
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello World!");
+    }
+}
