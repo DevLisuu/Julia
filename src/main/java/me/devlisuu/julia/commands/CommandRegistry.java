f24ad4d236e11b83eca2a528fa90c24b6861e36b
Lisu <everyaccountholder@wp.pl>
2022-05-15 13:47:53 +0200

    Better slash command update method

--- src/main/java/me/devlisuu/julia/commands/CommandRegistry.java
+++ src/main/java/me/devlisuu/julia/commands/CommandRegistry.java
@@ -1,10 +1,13 @@
 package me.devlisuu.julia.commands;
 
 import me.devlisuu.julia.Julia;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
 public class CommandRegistry {
     public static void registerTestGuildCommands() {
-        Julia.testGuild.upsertCommand("uptime", "Julia's uptime").queue();
-        Julia.testGuild.upsertCommand("memory", "Julia's memory usage").queue();
+        Julia.testGuild.updateCommands()
+                .addCommands(Commands.slash("uptime", "Julia's uptime"))
+                .addCommands(Commands.slash("memory", "Julia's memory usage"))
+                .queue();
     }
 }
